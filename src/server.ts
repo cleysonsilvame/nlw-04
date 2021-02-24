706ffb7908f809b52f8f38aebc5a672a46619b7c
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-02-24 11:44:41 -0300

    Add UserController and User Model

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
-
-app.post('/users', (req, res) => {
-  res.json({ message: 'Os dados foram salvos com sucesso!' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log('Server is running!'));
