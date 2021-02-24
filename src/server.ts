cc6b03ca5345287bc111469ef9333ae2e31705c4
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-02-24 00:59:49 -0300

    add typeorm and migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express from "express";
+import 'reflect-metadata';
+import express from 'express';
+import './database';
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  res.json({ message: 'Hello World!' })
+app.get('/users', (req, res) => {
+  res.json({ message: 'Hello World!' });
 });
 
-app.post("/users", (req, res) => {
-  res.json({ message: 'Os dados foram salvos com sucesso!' })
+app.post('/users', (req, res) => {
+  res.json({ message: 'Os dados foram salvos com sucesso!' });
 });
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3000, () => console.log('Server is running!'));
