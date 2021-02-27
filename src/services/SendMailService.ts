dbf52af69fc8261b1b4a12597d238d27c30bf14b
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-02-27 14:36:45 -0300

    finalizes mailing and relationship

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,4 +1,14 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import handlebars from 'handlebars';
+import fs from 'fs';
+
+interface variableMailProps {
+  name: string;
+  title: string;
+  description: string;
+  user_id: string;
+  link: string;
+}
 
 class SendMailService {
   private client: Transporter;
@@ -17,11 +27,17 @@ class SendMailService {
     });
   }
 
-  async execute(to: string, subject: string, body: string) {
+  async execute(to: string, variables: variableMailProps, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+
+    const mailTemplateParse = handlebars.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
     const message = await this.client.sendMail({
       to,
-      subject,
-      html: body,
+      subject: variables.title,
+      html,
       from: 'NPS <noreplay@nps.com.br>',
     });
 
